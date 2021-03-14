66b1f86a773a5cb0798628cadd659e799c7e6c7f
loveyousomuch <loveyousomuch554@gmail.com>
2021-03-14 17:30:19 +0300

    syn

--- block-server/server.py
+++ block-server/server.py
@@ -1,27 +1,16 @@
 import socket
-import os
 
 HOST = "127.0.0.1"
-PORT = 8080
-#BUF_SIZE = 4096
+PORT = 50007
+BUF_SIZE = 4096
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
-s.listen(5)
-
-def send_msg(conn, msg):
-  msg = bytes(msg+'\n', 'utf-8')
-  conn.send(msg)
-
-while 1:
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+  s.bind((HOST, PORT))
+  s.listen(1)
   conn, addr = s.accept()
-  print(f"Client: {addr[0]}:{addr[1]}")
-
-  #conn.send(bytes('Hello, World!\n', 'utf-8'))
-  send_msg(conn, 'Hello, World!')  
-
-  # close client connection
-  conn.close()
-  print()
-
-s.close() 
+  with conn:
+    print(f'Conn: {addr[0]}:{addr[1]}')
+    while True:
+      data = conn.recv(1024)
+      if not data: break
+      conn.sendall(data)
