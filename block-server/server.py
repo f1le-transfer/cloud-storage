fec4dc46ee1c8b1f440945b46937ba46c1d436e8
loveyousomuch <loveyousomuch554@gmail.com>
2021-03-14 19:23:13 +0300

    server

--- block-server/server.py
+++ block-server/server.py
@@ -1,16 +1,49 @@
-import socket
+import socket 
+import threading
 
-HOST = "127.0.0.1"
-PORT = 50007
-BUF_SIZE = 4096
+# TODO: 
+# 1. Saving data in the filesystem like world1_15_12_27.chunk
+# 2. Method for receive data
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-  s.bind((HOST, PORT))
-  s.listen(1)
-  conn, addr = s.accept()
-  with conn:
-    print(f'Conn: {addr[0]}:{addr[1]}')
+HEADER = 64
+PORT = 5050
+SERVER = '127.0.0.1'
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+
+    connected = True
+    while connected:
+        # Get len from header
+        data_len  = conn.recv(HEADER).decode(FORMAT)
+        if data_len:
+            data_len = int(data_len)
+            # Rewrite to receive data in chunks ↓
+            data = conn.recv(4096).decode(FORMAT)
+            if data == DISCONNECT_MESSAGE:
+                connected = False
+
+            print(f"[{addr}] {data}")
+            conn.send("Data received".encode(FORMAT))
+
+    conn.close()
+        
+
+def start():
+    server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
     while True:
-      data = conn.recv(1024)
-      if not data: break
-      conn.sendall(data)
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+
+
+print("[STARTING] server is starting...")
+start()
