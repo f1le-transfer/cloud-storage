46cad0138e1313b73e76ab8d3f430a7b460069d8
loveyousomuch <loveyousomuch554@gmail.com>
2021-03-14 23:43:22 +0300

    receive file

--- block-server/server.py
+++ block-server/server.py
@@ -6,6 +6,7 @@ import threading
 # 2. Method for receive data
 
 HEADER = 64
+BUF_SIZE = 4096
 PORT = 5050
 SERVER = '127.0.0.1'
 ADDR = (SERVER, PORT)
@@ -15,35 +16,45 @@ DISCONNECT_MESSAGE = "!DISCONNECT"
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
-def handle_client(conn, addr):
-    print(f"[NEW CONNECTION] {addr} connected.")
 
-    connected = True
+def save_file(user_file, conn):
+  connected = True
+  with open(user_file, 'w+') as f:
     while connected:
-        # Get len from header
-        data_len  = conn.recv(HEADER).decode(FORMAT)
-        if data_len:
-            data_len = int(data_len)
-            # Rewrite to receive data in chunks ↓
-            data = conn.recv(4096).decode(FORMAT)
-            if data == DISCONNECT_MESSAGE:
-                connected = False
+      data = conn.recv(BUF_SIZE).decode(FORMAT)
+      f.write(data)
+      if data == DISCONNECT_MESSAGE:
+        connected = False
+      #f.write(data)
+  print('File saved.')
+  return connected
 
-            print(f"[{addr}] {data}")
-            conn.send("Data received".encode(FORMAT))
+def handle_client(conn, addr):
+  print(f"[NEW CONNECTION] {addr} connected.\n")
 
-    conn.close()
+  connected = True
+  while connected:
+    data_len = conn.recv(HEADER).decode(FORMAT)
+    if data_len:
+      connected = save_file('test_server.txt', conn)
+      #print(f"[{addr}] {data}")
+      conn.send("Data received".encode(FORMAT))
+
+  conn.close()
         
 
 def start():
-    server.listen()
-    print(f"[LISTENING] Server is listening on {SERVER}")
-    while True:
-        conn, addr = server.accept()
-        thread = threading.Thread(target=handle_client, args=(conn, addr))
-        thread.start()
-        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+  server.listen()
+  print(f"[LISTENING] Server is listening on {SERVER}")
+  while True:
+    conn, addr = server.accept()
+    thread = threading.Thread(target=handle_client, args=(conn, addr))
+    thread.start()
+    print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}\n")
 
 
 print("[STARTING] server is starting...")
 start()
+
+
+
