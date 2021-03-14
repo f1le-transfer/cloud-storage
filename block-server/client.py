56bb8f8fcbeea4f35675644f2a2d8ed766f7cd03
loveyousomuch <loveyousomuch554@gmail.com>
2021-03-14 19:24:36 +0300

    example client

---
+++ block-server/client.py
@@ -0,0 +1,31 @@
+# Client example
+import socket
+
+HEADER = 64
+PORT = 5050
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = "127.0.0.1"
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(4096).decode(FORMAT))
+
+send("Hello World!")
+
+input_msg = input()
+while input_msg != 'd':
+  send(input_msg)
+  input_msg = input()
+
+send(DISCONNECT_MESSAGE)
+client.close()
