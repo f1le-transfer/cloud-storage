38d33e3ba9a96581136d339d687e28c88a64f7d0
loveyousomuch <loveyousomuch554@gmail.com>
2021-03-19 00:06:10 +0300

    receive file without headers

--- block-server/server.py
+++ block-server/server.py
@@ -16,35 +16,28 @@ DISCONNECT_MESSAGE = "!DISCONNECT"
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
-reserved_fields = ['TCP', 'OK', 'Content-Length', 'Content-Type', 'Date', 'Server']
+#reserved_fields = ['TCP', 'OK', 'Content-Length', 'Content-Type', 'Date', 'Server']
 
 def save_file(user_file, data):
   with open(user_file, "+w") as f:
     f.write(data) 
 
 def handle_client(conn, addr):
-  print(f"[NEW CONNECTION] {addr} connected.\n")
+  print(f"\n[NEW CONNECTION] {addr} connected.")
 
   connected = True
   while True:
-    # Receive headers
-    data_len = conn.recv(HEADER).decode(FORMAT).strip()
-    req_type = conn.recv(HEADER).decode(FORMAT).strip()
-    file_name = "NO"#conn.recv(HEADER).decode(FORMAT).strip()
-    
-      
-    if not data_len: break
-    print(f'Len: {data_len} {req_type}')
-    
     # Get data from client
     data = conn.recv(BUF_SIZE).decode(FORMAT)
-    print(f'[{addr}] TCP: {req_type}, FILE: {file_name}, Data from client:\n{data}')
+    print(f"[{addr}] Data from client:\n{data}")
     
+    if not data: break
+
     # Save data in file
-    #save_file('server_test.txt', data)
+    save_file('server_test.txt', data)
     
     # Send message about end of the downloading
-    conn.send("Data received".encode(FORMAT))
+    #conn.send("Data received".encode(FORMAT))
 
   conn.close()
         
