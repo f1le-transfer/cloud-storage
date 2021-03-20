d38049978bac9e7edaca9d54607e54ee6b3ddc6b
loveyousomuch <loveyousomuch554@gmail.com>
2021-03-20 11:43:02 +0300

    send and receive chunk between cliet and server

--- block-server/server.py
+++ block-server/server.py
@@ -1,5 +1,6 @@
 import socket 
 import threading
+import os
 
 # TODO: 
 # 1. Saving data in the filesystem like world1_15_12_27.chunk
@@ -21,8 +22,8 @@ def header(data):
   return data
 
 # Send header
-def send_msg(conn, _chunk_name):
-  _header = header(f"Connection: close\nChunk-name: {_chunk_name}")
+def send_header(conn, _chunk_name):
+  _header = header(f"Chunk-name: {_chunk_name}\nContent-length: {0}")
   conn.send(_header)
 
 # Parse header to the object
@@ -40,6 +41,29 @@ def save_file(user_file, data):
   with open(user_file, "+w") as f:
     f.write(data)
 
+def send(conn, msg, req_type, file_size, path_file = None):
+  file_size = "Content-length: {0}\n".format(file_size)
+  _header = "TCP {0}\nChunk-name: {1}\n".format(req_type, path_file)
+    
+  if file_size:
+    conn.send(header(_header + file_size))
+  else:
+    conn.send(header(_header + "File-path: {0}".format(path_file)))
+  
+  if msg:
+    msg = msg.encode(FORMAT)
+    conn.send(msg)
+
+# Send chunk
+def send_file(conn, name, req_type, chunk_name):
+  size = os.path.getsize(name)
+  f = open(name, '+r')
+  l = f.read(BUF_SIZE)
+  while (l):
+    send(conn, l, req_type, size, chunk_name)
+    l = f.read(BUF_SIZE)
+  f.close()
+
 def handle_client(conn, addr):
   print(f"\n[NEW CONNECTION] {addr} connected.\n")
   _method = _chunk_name = None
@@ -48,25 +72,34 @@ def handle_client(conn, addr):
   while True:
     # Get data from client
     header = conn.recv(HEADER).decode(FORMAT)
-    data = conn.recv(BUF_SIZE).decode(FORMAT)
+    if not header: break
     
     header = [h.strip() for h in header.split("\n")]
-        
-    if not data or not header: break
-        
+    
     _chunk_name = parase_header(header)["chunk_name"]
     _method = parase_header(header)["method"]
     
     if _method == "POST":
+      data = conn.recv(BUF_SIZE).decode(FORMAT)
+      if not data: break
+    
       # Save data in file
       save_file(_chunk_name, data)
+    elif _method == "GET":
+#      send_header(conn, _chunk_name)
+      send_file(conn, _chunk_name, "GET", _chunk_name)
+      break
     else:
       isok = False
       break
+  conn.close()
   
   conn.close()
   if isok:
-    print(f"[LOG] File {_chunk_name} saved. Method: {_method}")
+    if _method == "POST":
+      print(f"[LOG] Method: {_method}. File \"{_chunk_name}\" saved.")
+    else:
+      print(f"[LOG] Method: {_method}. File \"{_chunk_name}\" sent.")
   else:
     print(f"[LOG] Wrong method \"{_method}\" for {_chunk_name}")
 
