1a40e3f03c9a2a33b815876e90d609bc3819acb6
loveyousomuch <loveyousomuch554@gmail.com>
2021-04-01 17:58:27 +0300

    server..

--- block-server/server.py
+++ block-server/server.py
@@ -1,20 +1,33 @@
-# WS server that sends messages at random intervals
-
 import asyncio
 import websockets
 import json
 
-async def consumer_handler(websocket, path):
-    host, port = websocket.remote_address
-    print(f'[Connection open] {host}:{port}')
-    async for message in websocket:
-        msg = json.loads(message)
-        print(f'{host}:{port}', msg['msg'])
-        if msg['msg'] == 'close':
-            await websocket.close()
-    print(f'[Connection close] {host}:{port}')
+async def handle(websocket, msg):  
+  if 'header' in msg: 
+    print('Header:', msg)  
+  elif 'msg' in msg:
+    if msg['msg'] == 'close':
+      await websocket.close()
+    else:
+      if len(msg['msg']) < 250:
+        print(msg['msg'])
+        await websocket.send(json.dumps({'status': '200', 'msg': msg['msg']}))
+      else:
+        print('Msg received.')
+        await websocket.send(json.dumps({'status': '200', 'msg': 'Msg recv.'}))
 
-start_server = websockets.serve(consumer_handler, "127.0.0.1", 5050)
+
+async def consumer_handler(websocket, path):
+  host, port = websocket.remote_address
+  print(f'[Connection open] {host}:{port}')
+  async for message in websocket:
+    print(len(message))
+    print()
+    msg = json.loads(message)
+    await handle(websocket, msg)                
+  print(f'[Connection close] {host}:{port}')
+
+start_server = websockets.serve(consumer_handler, host="127.0.0.1", port=5050, read_limit=4096, max_size=4096, max_queue=10)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
