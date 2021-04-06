94bc9f140f929cbd7c8ede5df09884725725672f
loveyousomuch <loveyousomuch554@gmail.com>
2021-04-06 11:15:43 +0300

    recv and sent sdp

--- block-server/server.py
+++ block-server/server.py
@@ -1,33 +1,107 @@
+import json
 import asyncio
 import websockets
-import json
+import uuid
 
-async def handle(websocket, msg):  
-  if 'header' in msg: 
-    print('Header:', msg)  
-  elif 'msg' in msg:
-    if msg['msg'] == 'close':
-      await websocket.close()
-    else:
-      if len(msg['msg']) < 250:
-        print(msg['msg'])
-        await websocket.send(json.dumps({'status': '200', 'msg': msg['msg']}))
-      else:
-        print('Msg received.')
-        await websocket.send(json.dumps({'status': '200', 'msg': 'Msg recv.'}))
+from aiortc import MediaStreamTrack, RTCPeerConnection, RTCSessionDescription, RTCIceCandidate
 
+host = '127.0.0.1'
+port = 5050
+# set of ...
+pcs = set()
+peerConnection = None
 
-async def consumer_handler(websocket, path):
-  host, port = websocket.remote_address
-  print(f'[Connection open] {host}:{port}')
-  async for message in websocket:
-    print(len(message))
+class clsprops:
+	def __init__(self, data):
+		self.__dict__ = data
+
+async def offer_handler(msg, remote, ws):
+    global peerConnection
+    # Create peer connection
+
+    config = {
+        'iceServers': [clsprops({'urls': 'stun:stun.l.google.com:19302'})],
+    }
+
+    peerConnection = RTCPeerConnection(clsprops(config))
+    pc_id = "[PeerConnection(%s)]" % uuid.uuid4()
+    pcs.add(peerConnection)
+
+    params = json.loads(msg['offer'])
+    remote_offer = RTCSessionDescription(sdp=params["sdp"], type=params["type"])
+
+    # Handle offer
+    await peerConnection.setRemoteDescription(remote_offer)
+
+    # Create answer
+    answer = await peerConnection.createAnswer()
+    await peerConnection.setLocalDescription(answer)
+
+    # Send answer
+    await ws.send(json.dumps(
+        { 'offer': {"sdp": answer.sdp, "type": answer.type} }
+    ))
+
+    def log_info(msg, *args):
+        print(pc_id + ' ' + msg, *args)
+    
+    log_info('Created for %s', remote)
+
+    @peerConnection.on("datachannel")
+    def on_datachannel(channel):
+        @channel.on("message")
+        def on_message(message):
+            if isinstance(message, str) and message.startswith("ping"):
+                channel.send("pong" + message[4:])
+
+    @peerConnection.on("connectionstatechange")
+    async def on_connectionstatechange():
+        log_info(f"Connection state is {peerConnection.connectionState}")
+        if peerConnection.connectionState == "failed":
+            await peerConnection.close()
+            pcs.discard(peerConnection)
+
+async def ice_handler(ice, ws):
+    print(ice)
     print()
-    msg = json.loads(message)
-    await handle(websocket, msg)                
-  print(f'[Connection close] {host}:{port}')
+    await peerConnection.addIceCandidate(clsprops(ice))
+    None
 
-start_server = websockets.serve(consumer_handler, host="127.0.0.1", port=5050, read_limit=4096, max_size=4096, max_queue=10)
+# handle message from client
+async def msg_handler(ws, msg, remote):
+    msg = json.loads(msg)
+    if 'msg' in msg:
+        if msg['msg'] == 'close':
+            await ws.close()
+        else:
+            print('msg', msg['msg'])
+            await ws.send(json.dumps({
+            'status': 200,
+            'msg': msg['msg']
+        }))
+    elif 'offer' in msg:
+        await offer_handler(msg, remote, ws)
+        await ws.send(json.dumps({
+            'status': 'handle peer connection'
+        }))
+    elif 'new-ice-candidate' in msg:
+        # await peerConnection
+        await ice_handler(json.loads(msg['new-ice-candidate']), ws)
+        await ws.send(json.dumps({
+            'status': 'add ice candidate'
+        }))
 
+async def consumer_handler(ws, path):
+    host, port = ws.remote_address
+    remote = f"{host}:{port}"
+    print(f'[Connection open] {remote}')
+    async for msg in ws:
+        await msg_handler(ws, msg, remote)
+    print(f'[Connection close] {host}:{port}')
+
+start_server = websockets.serve(consumer_handler, host, port)
+print(f'[MSG] Websocket server listen on {host}:{port}')
+
+# run websocker server
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
