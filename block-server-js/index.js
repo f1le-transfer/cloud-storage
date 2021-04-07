0d73f1fb0db0ed571e6ced4ec900507cf4170c8b
loveyousomuch <loveyousomuch554@gmail.com>
2021-04-08 00:58:11 +0300

    recv file metadata

--- block-server-js/index.js
+++ block-server-js/index.js
@@ -2,6 +2,7 @@ const { RTCPeerConnection, RTCSessionDescription } = require('wrtc')
 const { server: WebSocketServer } = require('websocket')
 const http = require('http');
 const fs = require('fs')
+const path = require('path')
 
 const httpServer = http.createServer((req, res) => { res.statusCode = 404; res.end('Not found') })
 httpServer.listen(5050, () => console.log('Server run on 5050'))
@@ -20,20 +21,25 @@ ws_server.on('request', (req) => {
     connection.on('close', close_handler.bind(undefined, req.origin))
 })
 
+const work_dir = 'files'
 let peerConnection;
 let writeStream;
 
+// TODO:
+// 1. Close write stream after file end
+// 2. Work with file system
+
 function msg_handler(origin, connection, { utf8Data: msg }) {
-    log('msg')
     msg = JSON.parse(msg)
-
     if (msg.offer) {
-        offer_handler(JSON.parse(msg.offer), connection)
+        log('Offer received')
+        return offer_handler(JSON.parse(msg.offer), connection)
     }
-
     if (msg['new-ice-candidate']) {
-        ice_handler(JSON.parse(msg['new-ice-candidate']), connection)
+        log('Candidate received')
+        return ice_handler(JSON.parse(msg['new-ice-candidate']), connection)
     }
+    log(msg)
 }
 
 async function offer_handler(offer, connection) {
@@ -61,28 +67,45 @@ function close_handler(origin, code, desc) {
 
 function set_peerConnection_events() {
     peerConnection.addEventListener('datachannel', event => {
-        const dataChannel = event.channel
-        writeStream = fs.createWriteStream('test.txt')
-        console.log('[DATA CHANNEL]', event.channel)
-
-
-        dataChannel.addEventListener('message', (e) => {
+        // Get data channel from client
+        const receiveChannel = event.channel
+        receiveChannel.addEventListener('message', (e) => {
             const msg = e.data
+            if (typeof msg === 'string') {
+                return createWriteStream(msg)
+            }
+
             if (typeof msg == 'object') {
-                console.log('[MSG]', msg.byteLength)
+                console.log('[receiveChannel MSG]', 'length', msg.byteLength)
                 return writeData(msg)
             }
-            console.log(msg)
         })
-    
-        dataChannel.addEventListener('open', console.log);
-        dataChannel.addEventListener('close', console.log);
-        dataChannel.addEventListener('error', console.error);
-    });
+
+        const onReceiveChannelStateChange = ({ type }) => console.log(`[receiveChannel] ${type}`)
+        receiveChannel.addEventListener('open', onReceiveChannelStateChange)
+        receiveChannel.addEventListener('close', onReceiveChannelStateChange)
+        receiveChannel.addEventListener('error', onReceiveChannelStateChange)
+    })
+    peerConnection.addEventListener('iceconnectionstatechange', () =>  console.log('[peerConnection]', 'iceConnectionStateChange:', peerConnection.iceConnectionState))
+    peerConnection.addEventListener('signalingstatechange', () => console.log('[peerConnection]', 'signalingStateChange:', peerConnection.signalingState))
+}
+
+function createWriteStream(msg) {
+    let file;
+    try {
+        file = JSON.parse(msg)
+    } catch (error) {
+        return console.error('[ERROR] prase json string from client')
+    }
+    console.log('[receiveChannel MSG]', file)
+
+    // Create work directory if not exist
+    fs.mkdir(work_dir, { recursive: true }, console.error)
+
+    writeStream = fs.createWriteStream(path.join(work_dir, file.name))
 }
 
 function writeData(buffer) {
     const data_Uint8Array = new Uint8Array(buffer)
-    console.log(typeof data_Uint8Array)
     writeStream.write(data_Uint8Array, 'base64')
 }
